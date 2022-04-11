0850362320803a30e44aa55efa7d9f3c398add1a
Lexi Dorrity <lexidorrity@Lexis-MacBook-Pro.local>
2022-04-11 14:10:34 -0700

    couple of css changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const { Recipe } = require("../models");
 const withAuth = require("../utils/auth");
 
 // Prevent non logged in users from viewing the homepage
-router.get("/", withAuth, async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const recipeData = await Recipe.findAll({});
 
